34f3cbc7833b9ea5479ee58786c6c4a4c6adbcd6
Hans Diepeveen <hansdiepeveen@hotmail.com>
2026-09-17 17:13:55 +0200

    test: guard delete category dependencies

--- backend/tests/MyShop.Application.Tests/Catalog/DeleteCategory/DeleteCategoryTests.cs
+++ backend/tests/MyShop.Application.Tests/Catalog/DeleteCategory/DeleteCategoryTests.cs
@@ -8,6 +8,15 @@ namespace MyShop.Application.Tests.Catalog.DeleteCategory;
 public sealed class DeleteCategoryTests
 {
     [Fact]
+    public void Constructor_RejectsNullDependencies()
+    {
+        var store = new StoreFake();
+        Assert.Throws<ArgumentNullException>(() => new UseCase(null!, store, store));
+        Assert.Throws<ArgumentNullException>(() => new UseCase(store, null!, store));
+        Assert.Throws<ArgumentNullException>(() => new UseCase(store, store, null!));
+    }
+
+    [Fact]
     public async Task ExecuteAsync_UnusedCategory_IsDeleted()
     {
         var store = new StoreFake { Category = Category.CreateRoot("Old") };
