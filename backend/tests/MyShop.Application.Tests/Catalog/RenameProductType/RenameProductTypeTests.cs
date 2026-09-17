4d74014ac86d09ff4c8039fde2a9305a66f1f3ec
Hans Diepeveen <hansdiepeveen@hotmail.com>
2026-09-17 17:13:55 +0200

    test: guard rename product type dependencies

--- backend/tests/MyShop.Application.Tests/Catalog/RenameProductType/RenameProductTypeTests.cs
+++ backend/tests/MyShop.Application.Tests/Catalog/RenameProductType/RenameProductTypeTests.cs
@@ -8,6 +8,14 @@ namespace MyShop.Application.Tests.Catalog.RenameProductType;
 public sealed class RenameProductTypeTests
 {
     [Fact]
+    public void Constructor_RejectsNullDependencies()
+    {
+        var store = new StoreFake();
+        Assert.Throws<ArgumentNullException>(() => new UseCase(null!, store));
+        Assert.Throws<ArgumentNullException>(() => new UseCase(store, null!));
+    }
+
+    [Fact]
     public async Task ExecuteAsync_RenamesAndPersistsExistingProductType()
     {
         var store = new StoreFake { ProductType = ProductType.Create("Old") };
